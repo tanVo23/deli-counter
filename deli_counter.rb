c4f1db78a0949edef9a9f691bcaf73ffda265afa
Tan Vo <tan10tvo@gmail.com>
2021-03-03 18:26:13 -0700

    commit tree 46c3faab152f8dc6b50f44b0baadc70a9a243111

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(name)
+    if name.length == 0
+        puts "The line is currently empty."
+    else
+        line_length ="The line is currently:"
+        name.each.with_index(1) do | waiting_person, place_in_line |
+            line_length << " #{place_in_line}. #{waiting_person}"
+        end
+        puts line_length
+    end
+end
+ 
+def take_a_number(people, name)
+    people << name
+    puts "Welcome, #{name}. You are number #{people.length} in line."
+end
+
+def now_serving(people)
+    if people.empty?
+        puts "There is nobody waiting to be served!" 
+    else
+        puts "Currently serving #{people.shift}."
+      end
+end
